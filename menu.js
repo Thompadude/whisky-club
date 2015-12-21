66c387f99052f0161e32661344c1cf128f0df583
Thomas Lansing <thompa.lansing@gmail.com>
2015-12-21 15:32:54 +0100

    finlira menyn

--- menu.js
+++ menu.js
@@ -4,17 +4,14 @@ var menuAnimation = function() {
 		$("#menu").animate(
 			{left: '0px'},
 			200);
-		$("#home").animate(
-			{left: '250px'},
-			200);
+		$("#iconOpenMenu").toggle(false, 1000);
+		$("#menu").toggle(true, 1000);
 	})
 	$("#iconCloseMenu").click(function(){
 		$("#menu").animate(
 			{left: '-250px'},
 			200);
-		$("#home").animate(
-			{left: '0px'},
-			200);		
+		$("#iconOpenMenu").toggle(50);
 	})
 };
 
