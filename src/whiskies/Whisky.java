8f40259d8372ea3274dfd0d8751f4cea98db21ec
Thompadude <thompa.lansing@gmail.com>
2015-12-22 21:15:14 +0100

    create super class for whiskies

---
+++ src/whiskies/Whisky.java
@@ -0,0 +1,50 @@
+package whiskies;
+
+public class Whisky {
+	String name;
+	String region;
+	String type;
+	String info;
+	Double alc;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getInfo() {
+		return info;
+	}
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+
+	public Double getAlc() {
+		return alc;
+	}
+
+	public void setAlc(Double alc) {
+		this.alc = alc;
+	}
+
+}
