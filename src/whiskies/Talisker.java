b4a75b774897eb263779540de5ae991f415061f8
Thompadude <thompa.lansing@gmail.com>
2016-01-02 21:04:05 +0100

    fix broken talisker

--- src/whiskies/Talisker.java
+++ src/whiskies/Talisker.java
@@ -3,27 +3,13 @@ package whiskies;
 public class Talisker extends Whisky {
 
 	public Talisker() {
-		super("talisker", "Talisker", "Islay Whisky", "Scotland", "Single Malt Whisky",
+		super("talisker", "Talisker 10 Year Old", "Isle of Skye", "Scotland", "Single Malt Whisky",
 
-				"Released in 2004, this bottling was aged for around five "
-						+ "years before being finished in a quarter cask for several "
-						+ "months, the size of the cask is quite small, thus does not "
-						+ "require such a long maturation. This remains a truly great achievement from Laphroaig."
+				"A massive success as the island representative in Diageo's 'Classic Malts'"
+						+ "series, Talisker 10yo's profile keeps increasing as more fans discover "
+						+ "its intense coastal spicy, peaty character. A truly elemental malt."
 
-		, "../images/laphroaigquartercask.jpg", 48, 0);
+		, "../images/talisker.jpg", 45.8, 0);
 	}
-	
-	/*
-	 	Whisky talisker = new Whisky();
-		talisker.setId("talisker");
-		talisker.setName("Talisker");
-		talisker.setRegion("Isle of Skye");
-		talisker.setCountry("Scotland");
-		talisker.setType("Single Malt Whisky");
-		talisker.setImgUrl("talisker.jpg");
-		talisker.setInfo("Write some info here please");
-		talisker.setAlc(45.8);
-		talisker.setGrade(0);
-	 */
 
 }
