f743506d41d695a6abd98f33efcac2af28cb4b89
Thompadude <thompa.lansing@gmail.com>
2016-01-02 21:09:02 +0100

    add comment to WhiskyHandler.java

--- src/whiskies/WhiskyHandler.java
+++ src/whiskies/WhiskyHandler.java
@@ -2,10 +2,14 @@ package whiskies;
 
 import java.util.ArrayList;
 
+/**
+ * Class serves as a database for all whiskies.
+ */
+
 public class WhiskyHandler {
-	
+
 	private ArrayList<Whisky> whiskies = new ArrayList<>();
-	
+
 	public WhiskyHandler() {
 		this.whiskies.add(new Talisker());
 		this.whiskies.add(new LaphroaigQuarterCask());
