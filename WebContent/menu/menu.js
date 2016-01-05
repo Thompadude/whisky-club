07f3cf72c49d0445fa3987e6bf6063f374ed406e
Thomas Lansing <thompa.lansing@gmail.com>
2016-01-06 00:16:55 +0100

    nothing added, just experimental code in comment

--- WebContent/menu/menu.js
+++ WebContent/menu/menu.js
@@ -7,22 +7,44 @@
  * While the menu is open an overlay fades the rest of the screen,
  * thus putting the focus on the menu.
  */
-$(document).ready( 
+var $closeMenu = function() {
+	$("#menu").animate({
+		left : '-225px'
+		}, 200);
+	$("#iconOpenMenu").toggle(50);
+	$(".overlay").fadeOut('slow'); }
 
+var $openMenu = function() {
+	$("#menu").animate({
+		left : '0px'
+	}, 200);
+	$("#iconOpenMenu").toggle(false, 1000);
+	$("#menu").toggle(true, 1000);
+	$(".overlay").fadeIn('slow'); }
+
+$(document).ready( 		
+		
 function() {
 	$("#iconOpenMenu").click(function() {
-		$("#menu").animate({
-			left : '0px'
-		}, 200);
-		$("#iconOpenMenu").toggle(false, 1000);
-		$("#menu").toggle(true, 1000);
-		$(".overlay").fadeIn('slow');
+		$openMenu();
 	})
 	$("#iconCloseMenu").click(function() {
-		$("#menu").animate({
-			left : '-225px'
-		}, 200);
-		$("#iconOpenMenu").toggle(50);
-		$(".overlay").fadeOut('slow');
+		$closeMenu();
 	})
+	
+/*
+ * 
+ * UTVECKLA DENNA KOD 
+ 	
+	$('#home').click (function () {
+		setTimeout(function (event) {
+			$closeMenu();
+			event.preventDefault();
+			var goTo = this.getAttribute("href");
+			setTimeout(function(){
+				window.location = goTo;}
+				,3000);
+		})
+	})
+ */
 });
