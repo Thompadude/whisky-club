4ca0e26521f1bd34065fd279102fe11ca5077e92
Thomas Lansing <thompa.lansing@gmail.com>
2016-01-07 09:58:33 +0100

    add comments to smoothPage.js

--- WebContent/smoothPage.js
+++ WebContent/smoothPage.js
@@ -1,18 +1,28 @@
-$('a[href*=#]').on('click', function(event){     
-    event.preventDefault();
-    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
-});
+$(document).ready(function() {
 
-// Fade the whisky on hover.
-$('.whiskywrapper').hover(
-	function() {
-		$(this).find('.whiskytitle').stop(true, true).fadeIn('slow');
-		$(this).find('.whiskygrade').stop(true, true).fadeIn('slow');
-		$(this).find('.img-responsive').stop(true, true).fadeTo('slow', 0.5);
-	},
-	function() {
-		$(this).find('.whiskytitle').stop(true, true).fadeOut('slow');
-		$(this).find('.whiskygrade').stop(true, true).fadeOut('slow');
-		$(this).find('.img-responsive').stop(true, true).fadeTo('slow', 1);
-	}	
-);
+	/**
+	 * When clicking on a-element, first prevent default behavior on links starting with #
+	 * Then follow the link with an animation, resulting in a smooth scrolling effect.
+	 */
+	$('a[href*=#]').on('click', function(event){     
+		event.preventDefault();
+		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+	})
+	
+	/**
+	 * Fade the whisky on hover.
+	 * The stop method prevents the animation to loop if hovering too much.
+	 */
+	$('.whiskywrapper').hover(
+		function() {
+			$(this).find('.whiskytitle').stop(true, true).fadeIn('slow');
+			$(this).find('.whiskygrade').stop(true, true).fadeIn('slow');
+			$(this).find('.img-responsive').stop(true, true).fadeTo('slow', 0.5);
+		},
+		function() {
+			$(this).find('.whiskytitle').stop(true, true).fadeOut('slow');
+			$(this).find('.whiskygrade').stop(true, true).fadeOut('slow');
+			$(this).find('.img-responsive').stop(true, true).fadeTo('slow', 1);
+		})
+		
+});
