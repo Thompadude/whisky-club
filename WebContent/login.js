d6a151ffc479e1f04b7b4f44110c9dc0efabd93b
Thomas Lansing <thompa.lansing@gmail.com>
2016-01-09 00:27:20 +0100

    small fixes to login

--- WebContent/login.js
+++ WebContent/login.js
@@ -1,6 +1,18 @@
-$(document).ready(
-	function() {
-		$('#loginjumobtron').fadeIn('slow');
-	}
-
-);
+$(document).ready(function() {
+		
+	$('#loginjumobtron').fadeIn('slow');
+		
+	$('form').submit(function(event){
+		// Caches a reference to the form before preventing default behavior.
+        form = this;
+		event.preventDefault();
+		$('#loginjumobtron').fadeOut(2000);
+		setTimeout(function(){
+			form.submit();
+		}, 3000
+		);
+		var userName = $('input[name=usrName').val();
+		$('#loginwelcomemsg').append("Hello, " + userName + "!<br>Authenticating...")
+		$('#loginwelcomemsg').fadeIn('slow');
+	})
+});
