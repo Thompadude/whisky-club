578414e6a5ebd6741453f9c43cf69e688fdb1cff
Thomas Lansing <thompa.lansing@gmail.com>
2016-01-08 22:03:59 +0100

    add small hover effect on index page

---
+++ WebContent/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('.indexdiv').hover(function(){
+		$(this).find('img').stop(true, true).fadeTo('slow', 1)
+		$(this).find('p').stop(true, true).animate({fontSize: '400%'}, 'medium')
+	},
+	function(){
+		$(this).find('img').stop(true, true).fadeTo('slow', .6)
+		$(this).find('p').stop(true, true).animate({fontSize: '200%'}, 'slow')
+	});
+});
