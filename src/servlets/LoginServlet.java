468edeef111257ed5cff08a68e42b1b7d57b4e91
Thomas Lansing <thompa.lansing@gmail.com>
2016-01-09 23:05:54 +0100

    refactor and add comments to LoginSerlvet.java

--- src/servlets/LoginServlet.java
+++ src/servlets/LoginServlet.java
@@ -2,27 +2,21 @@ package servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import database.Data;
-import database.WhiskyDatabase;
 import management.LoginHandler;
-import whiskies.Whisky;
 
 /**
  * Servlet implementation class Main
  */
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
+	
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
@@ -31,24 +25,22 @@ public class LoginServlet extends HttpServlet {
 	}
 	
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
 		request.setCharacterEncoding("UTF-8");
 		
-		HttpSession session = request.getSession();
-		PrintWriter out = response.getWriter();
-		
-		// Get a user name and a password from login.jsp
+		// Get the user name and password from AJAX.
 		String usrName = request.getParameter("ajaxUsrName");
 		String password = request.getParameter("ajaxPassword");
 
-		// Checks if login is valid.
+		// Check if login information is valid.
 		LoginHandler loginHandler = new LoginHandler();
 		boolean isSuccessfulLogin = loginHandler.authenticate(usrName, password);
-		// If login is OK, redirect to main page, if not - nothing happens.
+
+		// Prints out a message to the user depending on if login is successful or not.
+		PrintWriter out = response.getWriter();
 		if (isSuccessfulLogin) {
 			// The success print is important in login.js. The string is the key for redirection.
 			out.print("Login Successful!");
@@ -58,8 +50,7 @@ public class LoginServlet extends HttpServlet {
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
