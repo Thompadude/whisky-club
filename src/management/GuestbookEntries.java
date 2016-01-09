2ac3f084ce71ae781f78c332d081fd49c3af583a
Thomas Lansing <thompa.lansing@gmail.com>
2016-01-09 21:20:29 +0100

    add comments to GuestbookDatabase.java

--- src/management/GuestbookEntries.java
+++ src/management/GuestbookEntries.java
@@ -1,11 +1,21 @@
 package management;
 
+/**
+ * Gathers all guestbook entries.
+ */
 public class GuestbookEntries implements java.io.Serializable {
 
 	private String name;
 	private String entry;
 	private String date;
 
+	/**
+	 * Constructs a guestbook entry.
+	 * 
+	 * @param name is the author of the entry.
+	 * @param entry is the entry/comment.
+	 * @param date is the date the entry was written.
+	 */
 	public GuestbookEntries(String name, String entry, String date) {
 		this.name = name;
 		this.entry = entry;
