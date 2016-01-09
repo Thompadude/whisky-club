ab767cdaf044dcc2dc14e09dde2343c814fb559e
Thomas Lansing <thompa.lansing@gmail.com>
2016-01-09 20:21:21 +0100

    add comments to Data.java

--- src/database/Data.java
+++ src/database/Data.java
@@ -1,16 +1,26 @@
 package database;
 
+/**
+ * An assemblage of all data objects. The methods in this class is static
+ * to ensure to always refer to the same data.
+ */
 public class Data {
 
 	private static WhiskyDatabase whiskyDatabase = new WhiskyDatabase();
 	private static GuestbookDatabase guestbookDatabase = new GuestbookDatabase();
-	
+
+	/**
+	 * @return the WhiskyDatabase object containing all whiskies objects.
+	 */
 	public static WhiskyDatabase getWhiskyHandler() {
 		return whiskyDatabase;
 	}
-	
+
+	/**
+	 * @return the GusebookDatabase object containing all guestbook entries.
+	 */
 	public static GuestbookDatabase getGuestbookDatabase() {
 		return guestbookDatabase;
 	}
-	
+
 }
