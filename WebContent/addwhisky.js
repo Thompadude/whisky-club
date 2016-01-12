3a9b17fd0fb9f0923e87a0df61ea18257a67aabb
Thomas Lansing <thompa.lansing@gmail.com>
2016-01-12 09:48:44 +0100

    add awesome code to check add whisky form

--- WebContent/addwhisky.js
+++ WebContent/addwhisky.js
@@ -1,9 +1,22 @@
 $(document).ready(function(){
 	
+	var validateForm = function(){
+		var isFieldsFilled = true;
+		$('input').each(function(index){
+			if ($(this).val() === '' || $(this).val() === null) {
+				isFieldsFilled = false;
+				$('#result').html("Please fill in all forms");
+				return false;
+			}
+		});
+		return isFieldsFilled;;
+	}
+	
 	var $name, $region, $country, $type, $info, $imgurl, $alc;
 	
 	$('#submit').click(function() {
-	
+		var isCreatingNewWhisky = validateForm();
+		if (isCreatingNewWhisky) {
 		$name = $('#name').val();
 		$region = $('#region').val();
 		$country = $('#country').val();
@@ -22,6 +35,6 @@ $(document).ready(function(){
 					$('form').trigger("reset");
 					}
 			});
-	
+		}
 	})
 });
